13e02cf25fa64287531cb774e3cc74f53b6f79d0
Chakkaradeep (Chaks) CC <chakkaradeep@users.noreply.github.com>
2023-05-03 10:07:29 -0700

    Added Streamlit support. The agent will now generate the front-end using Streamlit.

--- pycodeagi-gpt4.py
+++ pycodeagi-gpt4.py
@@ -1,7 +1,7 @@
+import ast
 import configparser
 import os
 import re
-import ast
 from typing import List, Dict, Any
 
 from langchain import LLMChain
@@ -32,8 +32,10 @@ class GeneratePyCodeChain(LLMChain):
     @classmethod
     def create_chain(cls, verbose: bool = False) -> LLMChain:
         system_template = ("""
-            You are code generation AI proficient in Python.\n
-            Your goal is to build console-based Python app.\n 
+            You are code generation AI proficient in Python and Streamlit.\n
+            Your goal is to build a Python app.\n 
+            You will use Streamlit for building the app user interface.\n
+            Assume all required libraries are installed.\n
             {instructions}.""")
         system_prompt_template = PromptTemplate(template=system_template, input_variables=["instructions"])
         system_message_prompt = SystemMessagePromptTemplate(prompt=system_prompt_template)
@@ -45,7 +47,7 @@ class GeneratePyCodeChain(LLMChain):
         prompt = ChatPromptTemplate.from_messages([system_message_prompt, user_message_prompt])
         llm = ChatOpenAI(model_name="gpt-4",
                          temperature=0.35,
-                         request_timeout=180)
+                         request_timeout=240)
         chain_instance = cls(prompt=prompt, llm=llm)
         return chain_instance
 
@@ -70,15 +72,16 @@ class PyCodeAGI(Chain, BaseModel):
 
     def _call(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
         objective = inputs["objective"]
-        print("\033[93m" + "\n" + "*****OBJECTIVE*****" + "\033[0m")
+
+        print("\n" + "\033[93m" + "\n" + "*****OBJECTIVE*****" + "\033[0m")
         print(objective.strip())
         with open(output_file, "a") as f:
             f.write(f"Objective: \n {objective.strip()}\n\n")
 
         print("\033[93m" + "*****DESCRIPTION*****" + "\033[0m")
-        instructions = "Users will interact with the app in a console terminal."
+        instructions = f"Users will interact with the web app built using Streamlit and Python."
         tasks = f"""            
-            Create a concise description for the console-based Python app: {objective}\n
+            Create a concise description for the Python app: {objective}\n
             Use your expertise to envision the app's purpose and functionality.
         """
         self.llm_chain.llm.max_tokens = 200
@@ -163,12 +166,25 @@ class PyCodeAGI(Chain, BaseModel):
                         Code Flow:\n
                         {codeflow}
                         """
+        # GPT4 may not follow some coding guidelines and may hallucinate.
+        # Instruct the model specific guidelines to follow.
         tasks = f"""
             Write the Python code for the app in a single python file.\n
-            Avoid using database for backend storage, instead use in-memory options.\n
-            Exclude environment setup, testing, debugging, and deployment tasks.
+            Use SQLite python module for data storage .\n
+            Exclude environment setup, testing, debugging, and deployment tasks.\n
+            Build sample datasets with at least five items.\n
+            Follow these coding guidelines:
+            - Check and create database tables first in the main function.\n
+            - Use pd.loc to append new rows to the DataFrame.\n
+            ---Example: event_data.loc[len(event_data)] = sample_events.loc[0]\n
+            - When building date sliders:\n
+            ---First Convert dates using to_pydatetime()
+            ---Then use their min and max values in st.slider
+            - Use pd.to_datetime() on selected date ranges when filtering calendar events.
+            - Save all data in a SQLite database.
             """
-        self.llm_chain.llm.max_tokens = 4000
+
+        self.llm_chain.llm.max_tokens = 5000
         appcode = self.llm_chain.run(instructions=instructions, tasks=tasks)
         print(appcode.strip())
         with open(output_file, "a") as f:
@@ -206,11 +222,15 @@ if __name__ == "__main__":
     if os.path.exists(code_file):
         os.remove(code_file)
 
+    # Get the user input
+    print("\n" + "\033[93m" + "\n" + "Welcome to pyCodeAGI" + "\033[0m")
+    print("\nA simple agent that builds a Python app for you!\n")
+    print("The agent will use Streamlit to turn your Python app into a web app!\n")
+    print(u'\u2193' + " Lets get started " + u'\u2193' + "\n")
+    objective = input(f"What app do you want me to build: ")
+
     # Initialize our agent
     pycode_agi = PyCodeAGI.create_llm_chain()
-
-    # Get the user input
-    objective = input(f"\nWhat app do you want me to build: ")
 
     # Run the agent and witness the MAGIC!
     pycode_agi({"objective": objective})
